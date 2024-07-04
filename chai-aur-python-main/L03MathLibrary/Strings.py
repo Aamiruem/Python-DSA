be4d9af8018f980b29af7c71ebd84618520f2532
Aamir@786 <aamirhussainazad786@gmail.com>
2024-07-05 00:47:27 +0530

    commit add

---
+++ chai-aur-python-main/L03MathLibrary/Strings.py
@@ -0,0 +1,108 @@
+chai = 'masala chai'
+print(chai)
+
+slice_chai = chai[0:6]
+
+print(slice_chai)
+
+num_list = "0123456789"
+print(num_list[3:])
+
+print(num_list[:7])
+
+print(num_list[0:7:2])
+
+print(num_list[0:7:3])
+
+print(num_list[-1:])
+
+print(num_list[7:-1])
+
+print(num_list[0:-1])
+
+tea = 'Masala Chai'
+print(tea.lower())
+print(tea.upper())
+
+print(chai)
+
+
+
+chai = "  Masala Chai   "
+print(chai)
+
+
+chai = "Lemon Chai"
+print(chai.replace("Lemon", "Ginger"))
+
+
+chai = "Masala Chai"
+print(chai.find("Chai"))
+
+
+print(chai.find("mango"))
+
+# counting chai
+chai = "masala chai chai chai chai"
+print(chai.count("chai"))
+
+print(chai.count("a"))
+
+print(chai.count("i"))
+
+print(chai.count("e"))
+
+
+
+chai_type = "Masala"
+quantity = 2
+order = "I ordered {} cups of {} chai"
+print(order)
+print(f"You ordered {quantity} {chai_type} chai.")
+
+print(order.format(quantity, chai_type))
+
+
+chai_variety = ["Lemon", "Masala", "Ginger"]
+print(chai_variety)
+
+print("".join(chai_variety))
+
+print(" ".join(chai_variety))
+
+print("_".join(chai_variety))
+
+print("-".join(chai_variety))
+
+print(", ".join(chai_variety))
+
+
+print(len(chai))
+
+
+chai = "Masala Chai"
+for letter in chai:
+    print(letter)
+
+
+chai = "He said, \"Masala chai is awesome\" "
+print(chai)
+
+chai = 'He said, "Masala chai is awesome"'
+print(chai)
+
+
+chai = "MASALA\nChai"
+print(chai)
+
+chai = r"Masala\nchai"
+print(chai)
+
+
+chai = r"c:\\user\\pwd\\"
+print(chai)
+
+
+chai = r"c:\user\pwd"
+
+print(chai)
--- chai-aur-python-main/L03MathLibrary/octalLiteral.py
+++ chai-aur-python-main/L03MathLibrary/octalLiteral.py
@@ -1,26 +1,143 @@
-x = 0o20
-print(x)
-# output is 16
+# x = 0o20
+# print(x)
+# # output is 16
 
-y = 0xff
-print(y)
-# output is 255
-
-
-x = 0b1000
-print(x)
-# output is 8
-
-octal = oct(64)
-print(octal)
-# output is 0o100
+# y = 0xff
+# print(y)
+# # output is 255
 
 
-hex = hex(64)
-print(hex)
-# output is 0x40
+# x = 0b1000
+# print(x)
+# # output is 8
+
+# octal = oct(64)
+# print(octal)
+# # output is 0o100
 
 
-bin = bin(64)
-print(bin)
-# output is 0b1000000
+# hex = hex(64)
+# print(hex)
+# # output is 0x40
+
+
+# bin = bin(64)
+# print(bin)
+# # output is 0b1000000
+
+
+# x = int(3.14)
+# print(x)
+
+
+# x = float(314)
+# print(x)
+
+
+# x = bool(3.14)
+# print(x)
+
+
+# x != bool(3.14)
+# print(x)
+
+
+# x = int('64',  8)
+# print(x)
+
+
+# x = int('64',  16)
+# print(x)
+
+# x = int('10000',  2)
+# print(x)
+
+# x = 1
+# print(x<<2)
+
+
+
+# import random
+
+# # Generate a random integer between 1 and 100
+
+# random_integer = random.randint(1, 100)
+# print(random_integer)
+
+# l1 = ['lemon', 'masala', 'ginger', 'mint']
+
+# print(random.choice(l1))
+
+
+
+# l1 = ['Biryani', 'motton', 'polaw', 'Rice']
+# r = random.choice(l1)
+# print(r)
+
+
+# x = 0.1 + 0.1 + 0.4
+
+# print(x)
+
+
+
+# x = 0.1 + 0.1 + 0.1 - 0.4
+
+# print(x)
+# # output -0.09999999999999998
+
+
+# x = (0.1 + 0.1 + 0.1) - 0.4
+
+# print(x)
+# # output -0.09999999999999998
+
+
+# import math
+# x = (0.1 + 0.1 + 0.1) - 0.4
+# print(x)
+
+
+
+# # This is a correct way to find it
+# from decimal import Decimal
+# x = Decimal('0.1') + Decimal('0.1') + Decimal('0.1')
+
+# print(x - Decimal('0.4'))
+
+
+# from fractions import Fraction
+
+# x = Fraction(0.1) + Fraction(0.1) + Fraction(0.1)
+
+# print(Fraction(x))
+
+
+
+# from fractions import Fraction
+
+# x = Fraction(2, 7)
+
+# print(Fraction(x))
+
+
+
+
+#----------------------------#
+
+set1 = {1, 2, 3, 4}
+print(set1&{1, 3})
+
+
+print(set1 | {1, 3})
+
+print(set1 | {1, 3, 7})
+
+set2 = set1 - {1, 2, 3, 4}
+print(set2)
+
+print(type({}))
+# output set()
+# output <class 'dict'>
+print(type(set1))
+
