98831b9a50f56780163a4ab05151a4a4910c658e
Aamir@786 <aamirhussainazad786@gmail.com>
2024-07-07 00:35:18 +0530

    commit add

---
+++ chai-aur-python-main/L04List/List.py
@@ -0,0 +1,103 @@
+# tea_varieties = ["Black", "Green", "Oolong", "White"]
+# print(tea_varieties)
+
+# print(tea_varieties[-1])
+
+
+# print(tea_varieties[1:3])
+
+# print(tea_varieties[:2])
+
+# print(tea_varieties[2:])
+
+
+# tea_varieties[3] = "Herbal"
+
+# print(tea_varieties)
+
+
+# tea_varieties[1:2] = "Lemon"
+# print(tea_varieties)
+
+
+# tea_varieties = ["Black", "Green", "Oolong", "White"]
+# print(tea_varieties)
+
+# print(tea_varieties[1:2])
+
+# tea_varieties[1:2] = ["Lemon"]
+
+# print(tea_varieties)
+
+# print(tea_varieties[1:3])
+
+
+# tea_varieties[1:3] = ["green", "Masala"]
+# print(tea_varieties)
+
+
+# print(tea_varieties[1:1])
+
+
+# tea_varieties[1:1] = ["test", "test"]
+
+# print(tea_varieties)
+
+# print(tea_varieties[1:2])
+
+
+# print(tea_varieties[1:3])
+
+
+# tea_varieties[1:3] = []
+# print(tea_varieties)
+
+
+# for tea in tea_varieties:
+#     print(tea, end="->",)
+    
+    
+# print("\n")
+# print(tea_varieties)
+
+
+# if "Oolong" in tea_varieties:
+#     print("Yes I have Oolong Tea")
+    
+# else:
+#     print("No I don't have Oolong Tea")
+    
+# tea_varieties.append("Matcha")
+
+# print(tea_varieties)
+
+# print(tea_varieties.pop()) #remove from last element
+
+# print(tea_varieties)
+
+# print(tea_varieties.remove("green"))
+# print(tea_varieties)
+
+# print(tea_varieties.insert(1, "green"))
+
+# print(tea_varieties)
+
+# tea_varieties_copy = tea_varieties.copy()
+# print(tea_varieties_copy)
+
+# tea_varieties_copy.append("Black Tea")
+# print(tea_varieties_copy)
+# print(tea_varieties)
+
+
+x = range(10)
+print(x)
+
+squared_num = [x**2 for x in range(10)]
+
+print(squared_num)
+
+
+cube_num = [y**3 for y in range(10)]
+
+print(cube_num)
